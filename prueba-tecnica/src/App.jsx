da19c0114ceacbc329b5e83654d7e8e6cb9a4c0b
Samuel Hurtado Ortega <shurtado@aertecsolutions.com>
2024-02-22 09:29:52 +0100

    fixing errors

--- prueba-tecnica/src/App.jsx
+++ prueba-tecnica/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 
 // const cat_image_url = `https://cataas.com/cat/says/${firstThreeWords}?fontSize=50&fontColor=red&json=true`;
 const cat_fact = "https://catfact.ninja/fact";
@@ -43,25 +42,3 @@ export default function App() {
     </main>
   );
 }
-=======
-import "./App.css";
-
-function App() {
-  const [fact, setFact] = useState("cat fact");
-
-  useEffect(() => {
-    fetch("https://catfact.ninja/fact")
-      .then((res) => res.json())
-      .then((data) => setFact(data.fact));
-  }, []);
-
-  return (
-    <>
-      <h1>App de Gatitos</h1>
-      <p>{fact}</p>
-    </>
-  );
-}
-
-export default App;
->>>>>>> 7a408d76e308ce53a78d36066f5983bdbf8bcc6f
--- prueba-tecnica/src/main.jsx
+++ prueba-tecnica/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -8,15 +7,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </>
 );
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> 7a408d76e308ce53a78d36066f5983bdbf8bcc6f
